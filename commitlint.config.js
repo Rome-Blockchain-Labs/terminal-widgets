0a71601632ee0ffd5c44598601fe7f6f34243716
Ian de Jesus <ian@eskwelabs.com>
2022-02-14 10:41:47 +0900

    tool: update commitlint config to make rules flexible

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // update rules if you want to add more commit types
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        //tool - when adding and configuring dev dependencies
+        "tool",
+      ],
+    ],
+  },
+};
