188ab4f16aa8a79de427bc0bfea72b428fc336c1
iandjx <3683356+iandjx@users.noreply.github.com>
2022-03-03 08:35:38 +0900

    fix: enable cors

--- sherpa/next.config.js
+++ sherpa/next.config.js
@@ -1,26 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)?',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+          },
+        ],
+      },
+    ]
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
     config.resolve.fallback ==
       {
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        buffer: require.resolve("buffer"),
-        http: require.resolve("http-browserify"),
-        https: require.resolve("https-browserify"),
-        url: require.resolve("url"),
-        os: require.resolve("os-browserify/browser"),
-      };
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer'),
+        http: require.resolve('http-browserify'),
+        https: require.resolve('https-browserify'),
+        url: require.resolve('url'),
+        os: require.resolve('os-browserify/browser'),
+      }
     config.plugins.push(
       new webpack.ProvidePlugin({
-        process: "process/browser.js",
-        Buffer: ["buffer", "Buffer"],
+        process: 'process/browser.js',
+        Buffer: ['buffer', 'Buffer'],
       })
-    );
-    return config;
+    )
+    return config
   },
-};
+}
