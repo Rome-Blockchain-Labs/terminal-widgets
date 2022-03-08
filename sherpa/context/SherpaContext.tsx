0a198329290132136517bfde15033e25404f2a65
iandjx <3683356+iandjx@users.noreply.github.com>
2022-03-08 21:15:28 +0900

    fix: use ian-sherpasdk-beta for relayer withdraw

--- sherpa/context/SherpaContext.tsx
+++ sherpa/context/SherpaContext.tsx
@@ -1,66 +1,69 @@
-import React, { createContext, ReactNode, useEffect } from "react";
-import { useWeb3React } from "@web3-react/core";
-import { useState } from "react";
-import { injected } from "../connectors";
-import * as sherpa from "@iandjx/sherpasdk";//todo use real sdk
-const netId = 43113;//todo config
+import React, { createContext, ReactNode, useEffect } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { useState } from 'react'
+import { injected } from '../connectors'
+// import * as sherpa from "@iandjx/sherpasdk";//todo use real sdk
+// import * as sherpa from '@rome/sherpasdk'
+import * as sherpa from 'ian-sherpasdk-beta'
+
+const netId = 43113 //todo config
 const AVAXContracts = [
   {
     val: 10,
-    address: "0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f",
+    address: '0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f',
   },
   {
     val: 100,
-    address: "0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f",
+    address: '0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f',
   },
   {
     val: 500,
-    address: "0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f",
+    address: '0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f',
   },
-];
+]
 
 const defaultValue = {
   sherpaClient: null,
-  sherpaRelayerOptions:[],
+  sherpaRelayerOptions: [],
   AVAXContracts,
-};
-export const SherpaContext = createContext(defaultValue);
+}
+export const SherpaContext = createContext(defaultValue)
 
 interface SherpaContextProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const SherpaContextProvider = ({ children }: SherpaContextProps) => {
-  const { library, active, activate } = useWeb3React();
-  const [sherpaClient, setSherpaClient] = useState<any>(null);
-  const [ sherpaRelayerOptions, setSherpaRelayerOptions ] = useState<any>([])
+  const { library, active, activate } = useWeb3React()
+  const [sherpaClient, setSherpaClient] = useState<any>(null)
+  const [sherpaRelayerOptions, setSherpaRelayerOptions] = useState<any>([])
 
   useEffect(() => {
     injected.isAuthorized().then((isAuthorized) => {
       if (isAuthorized || window.ethereum) {
-        activate(injected);
+        activate(injected)
         // next line is a for for: https://giters.com/NoahZinsmeister/web3-react/issues/257
-        window?.ethereum?.removeAllListeners(["networkChanged"]);
+        window?.ethereum?.removeAllListeners(['networkChanged'])
       }
-    });
-  }, [activate]);
+    })
+  }, [activate])
 
   useEffect(() => {
     if (active) {
-      const client = new sherpa.SherpaSDK(netId, library);
+      const client = new sherpa.SherpaSDK(netId, library)
       setSherpaRelayerOptions(client.getRelayerList())
-      setSherpaClient(client);
+      setSherpaClient(client)
     }
-  }, [active, library]);
+  }, [active, library])
 
   const context = {
     sherpaClient,
     sherpaRelayerOptions,
     AVAXContracts,
-  };
+  }
   return (
     <SherpaContext.Provider value={context}>{children}</SherpaContext.Provider>
-  );
-};
+  )
+}
 
-export default SherpaContextProvider;
+export default SherpaContextProvider
