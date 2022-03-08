19d40c3324da1beec13c68604ad89feb227f75d6
iandjx <3683356+iandjx@users.noreply.github.com>
2022-03-08 12:54:01 +0900

    feat: added bridge sdk

---
+++ bridge-sdk/src/index.ts
@@ -0,0 +1,119 @@
+import { EventBus } from "@vmw/transport";
+
+declare global {
+  interface Window {
+    transport?: any;
+  }
+}
+export class BridgeSDK {
+  bus: EventBus;
+  widgetID: string;
+
+  private constructor(widgetID: string, bus: EventBus) {
+    this.widgetID = widgetID;
+    this.bus = bus;
+  }
+
+  static async init(widgetID: string) {
+    console.log("init called");
+    if (typeof window === "undefined" || !window) {
+      console.log("client window", window);
+      throw new Error("You tried to import client-only code on the server.");
+    }
+    await import("./scripts/transport.umd.min.js");
+    if (!window.transport) throw new Error("Tranport bus not yet initiated");
+
+    const { TransportEventBus } = window.transport;
+    TransportEventBus.boot();
+
+    const bus = TransportEventBus.getInstance();
+
+    bus.enableMessageProxy({
+      protectedChannels: ["widget-rome"],
+      proxyType: "child", // runs as child.
+      parentOrigin: `http://localhost:3000/`,
+      acceptedOrigins: [
+        "http://localhost:3000", // local dev
+      ], // production
+      targetAllFrames: true,
+      targetSpecificFrames: [],
+    });
+    return new BridgeSDK(widgetID, bus);
+  }
+
+  startRespondStream({ messageHandlers }: Stream): void {
+    this.bus
+      .respondStream("widget-rome", undefined, this.widgetID)
+      .generate((request: Event) => {
+        const handler = messageHandlers.find(
+          (messageHandler) => request.type === messageHandler.event.type
+        );
+        if (!handler) return;
+        return handler.handler(request);
+      });
+  }
+  // waits for messages from parent terminal and acts on the message
+  startListenStream({ messageHandlers }: Stream): void {
+    this.bus
+      .listenStream("widget-rome", undefined, this.widgetID)
+      .handle((request: Event) => {
+        const handler = messageHandlers.find(
+          (messageHandler) => request.type === messageHandler.event.type
+        );
+        if (!handler) return;
+        handler.handler(request);
+      });
+  }
+
+  // sends message to the parent terminal
+  sendMessage({ message }: Message): void {
+console.log(this.bus.sendResponseMessage)
+              this.bus.sendResponseMessage("widget-rome", "send response message");
+    this.bus.sendRequestMessageWithId("widget-rome", message, this.widgetID);
+  }
+
+  // sends a request to the parent terminal expecting a response
+  sendRequest({ request, responseHandler }: Request): void {
+    this.bus
+      .requestOnceWithId(this.widgetID, "widget-rome", request)
+      .handle((response: boolean) => {
+        responseHandler(response);
+      });
+  }
+}
+
+export interface MessageHandler {
+  event: Event;
+  handler: (event: Event) => any;
+}
+export interface Stream {
+  messageHandlers: MessageHandler[];
+}
+export interface Request {
+  request: Event;
+  responseHandler: (success: boolean) => any;
+}
+
+export interface Message {
+  message: Event;
+}
+
+export type Bus = EventBus;
+
+type WidgetType = `${"EVENT_"}${string}`;
+type MessageType = `${"MESSAGE_"}${string}`;
+type RequestType = `${"REQUEST_"}${string}`;
+
+export type Type =
+  | "EVENT_ROME_WIDGET_CLOSE"
+  | "EVENT_ROME_WIDGET_ENLARGE"
+  | WidgetType
+  | MessageType
+  | RequestType;
+
+export interface Event {
+  type: Type;
+  timestamp: number;
+  broadcast: boolean;
+  payload: any | undefined;
+}
---
+++ bridge-sdk/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+const commonConfig = {
+  clean: true,
+  splitting: false,
+  dts: true,
+  sourcemap: true,
+};
+export default defineConfig([
+  {
+    entry: ["./src/index.ts"],
+    ...commonConfig,
+    format: ["cjs", "esm", "iife"],
+    outDir: "dist/",
+  },
+]);
