3a25f5d44552cd241953443dc8bc56615ec05abf
iandjx <3683356+iandjx@users.noreply.github.com>
2022-03-26 08:43:35 +0900

    feat: use updated rome-terminal library

--- sherpa/context/IframeContext.tsx
+++ sherpa/context/IframeContext.tsx
@@ -1,60 +1,30 @@
 import { useRouter } from 'next/router'
-import React, { useEffect, ReactNode, useRef, useState } from 'react'
-import { RomeBridge } from '@rbl/terminal-library/RomeBridge'
-
-enum MessageType {
-  EVENT_ROME_WIDGET_CLOSE = 'EVENT_ROME_WIDGET_CLOSE',
-  EVENT_ROME_WIDGET_ENLARGE = 'EVENT_ROME_WIDGET_ENLARGE',
-  EVENT_ROME_CUSTOM_EVENT = 'EVENT_ROME_WIDGET_CUSTOM_EVENT',
-}
-interface Message {
-  type: MessageType
-  timestamp?: number
-  broadcast?: boolean
-  payload?: any
-}
+import React, { useEffect, ReactNode } from 'react'
+import { widgetBridge, RomeEventType } from '@rome/terminal-library/romeBridge'
 
 const IframeContext = ({ children }: { children: ReactNode }) => {
   const router = useRouter()
-  const [bridge, setBridge] = useState<any>()
+
   useEffect(() => {
-    const loadBridge = async () => {
-      const { romeBridge } = await import(
-        '@rbl/terminal-library/RomeBridge/widget'
-      )
-      setBridge(romeBridge)
-    }
-
-    loadBridge()
-
-    window.addEventListener('message', (ev) => {
-      const homeRedirect = () => {
-        router.push('/')
-      }
-      const complianceRedirect = () => {
-        router.push('/compliance')
-      }
-      const event: Message = ev.data
-
-      if (event.type === MessageType.EVENT_ROME_CUSTOM_EVENT) {
-        if (event.payload.redirect === '/compliance') {
-          complianceRedirect()
+    widgetBridge.init()
+    widgetBridge.subscribe(
+      RomeEventType.TERMINAL_CLICK_BUTTON,
+      function (action: any) {
+        const homeRedirect = () => {
+          router.push('/')
         }
-
-        if (event.payload.redirect === '/') {
+        const complianceRedirect = () => {
+          router.push('/compliance')
+        }
+        if (action.payload.id === 'home') {
           homeRedirect()
         }
+        if (action.payload.id === 'compliance') {
+          complianceRedirect()
+        }
       }
-    })
+    )
   }, [router])
-
-  useEffect(() => {
-    if (bridge) {
-      bridge.subscribe('rome.terminal.click_button', function (payload: any) {
-        console.log(payload)
-      })
-    }
-  }, [bridge])
 
   return <>{children}</>
 }
