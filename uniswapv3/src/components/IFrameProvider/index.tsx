8682e64fcf6e370e95aa5e4cd809a0268c27f185
Ian de Jesus <ian.dejesus.md@gmail.com>
2022-06-29 16:22:03 +0800

    feat: remove unused routes, added romebridge provider

---
+++ uniswapv3/src/components/IFrameProvider/index.tsx
@@ -0,0 +1,34 @@
+import { RomeEventType, widgetBridge } from '@romeblockchain/bridge'
+import React, { ReactNode, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+interface IFrameContextState {
+  widgetBridge: typeof widgetBridge | null
+}
+
+export const IFrameContext = React.createContext<IFrameContextState>({
+  widgetBridge: null,
+})
+
+const IFrameProvider = ({ children }: { children: ReactNode }) => {
+  const history = useHistory()
+  useEffect(() => {
+    widgetBridge.init()
+    widgetBridge.subscribe(RomeEventType.TERMINAL_CLICK_BUTTON, function (action: any) {
+      switch (action.payload.id) {
+        case 'home':
+          history.push('/swap')
+          break
+        case 'pool':
+          history.push('/pool')
+          break
+        default:
+          break
+      }
+    })
+  }, [history])
+
+  return <IFrameContext.Provider value={{ widgetBridge }}>{children}</IFrameContext.Provider>
+}
+
+export default IFrameProvider
