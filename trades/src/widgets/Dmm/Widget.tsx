8fed804355382423dced0c55dd1e6cfc88b24125
Mark Camball <mark.camball@gmail.com>
2022-07-29 12:19:10 -0400

    fix: some buttons working

--- trades/src/widgets/Dmm/Widget.tsx
+++ trades/src/widgets/Dmm/Widget.tsx
@@ -1,12 +1,51 @@
-import React, { FC, memo } from 'react';
+import {RomeEventType, widgetBridge} from '@romeblockchain/bridge';
+import React, {FC, memo, useContext, useEffect} from 'react';
 
-import { Kyber } from '../../components/icons';
+import {Kyber} from '../../components/icons';
 import DmmApp from '../../dapps/dmm';
-import { WidgetCommonState } from '../../types';
+import {WidgetCommonState} from '../../types';
+import {DmmContext, DmmPage} from './DmmContext';
+
+
+enum Tabs {
+  SWAP='swap',
+  POOL='pool',
+  POOLFINDER='poolfinder',
+  POOLS='pools',
+  CREATEPOOL='createpool',
+  ADDLIQUIDITY='addliquidity',
+  REMOVELIQUIDITY='removeliquidity',
+}
 
 export const widgetNameKyber = 'KYBERSWAP';
 
 const Widget: FC<WidgetCommonState> = memo(() => {
+
+  const { setPage } = useContext(DmmContext);
+  useEffect(()=>{
+
+    // setTimeout(()=>{setPage(DmmPage.POOLS)},5000)
+
+      widgetBridge.init()
+      widgetBridge.subscribe(RomeEventType.TERMINAL_CLICK_BUTTON,(action:any)=>{
+        switch (action.payload.id) {
+          //todo settings
+          case Tabs.SWAP:
+            setPage(DmmPage.SWAP)
+            break;
+          case Tabs.POOLS:
+            setPage(DmmPage.POOLS)//todo this is broken
+            console.log('setting pools')
+            break;
+          case Tabs.POOL:
+            setPage(DmmPage.POOL)
+            break;
+          default:
+            break;
+        }
+      })
+
+  },[setPage])
   // const widget = useSelector((state) => widgetByIdSelector(state)(uid));
 
   // const widget = {
@@ -34,7 +73,6 @@ const Widget: FC<WidgetCommonState> = memo(() => {
   return (
     <div tw="w-full h-full relative bg-dark-500">
       <Kyber active isBackground />
-
       <div tw="h-full overflow-auto relative">
         <div tw="flex justify-center items-center p-4 min-h-full">
           <DmmApp />
