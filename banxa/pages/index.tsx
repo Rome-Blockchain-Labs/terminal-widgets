598252688df72417beddfe960546d3b14b6a47f9
iandjx <iandjx@gmail.com>
2022-08-15 12:26:48 +0800

    feat: add aspect ratio

--- banxa/pages/index.tsx
+++ banxa/pages/index.tsx
@@ -1,17 +1,19 @@
-import axios from "axios";
-import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import axios from 'axios'
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
-  useEffect(() => {
-    axios.get("/api/banxa/crypto-buy").then((response) => {
-      setData(response.data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("/api/banxa/crypto-buy").then((response) => {
+  //     setData(response.data);
+  //   });
+  // }, []);
 
-  return <div>{JSON.stringify(data)}</div>;
-};
+  // return <div>{JSON.stringify(data)}</div>;
+  return <div />
+  return <div className="h-screen w-screen bg-gradient-to-br from-[#12162e] via-[#3d5d6e] to-[#12162e] "></div>
+}
 
-export default Home;
+export default Home
