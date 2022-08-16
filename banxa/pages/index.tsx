9b3164dbbb09ed8099c684e79a53f981c080a29a
iandjx <iandjx@gmail.com>
2022-08-16 14:28:15 +0800

    feat: add hash email logic

--- banxa/pages/index.tsx
+++ banxa/pages/index.tsx
@@ -1,14 +1,33 @@
 import type { NextPage } from 'next'
+
+import { ExclamationCircleIcon } from '@heroicons/react/solid'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useAppContext } from '../context/AppProvider'
+import { useRouter } from 'next/router'
+
 const Home: NextPage = () => {
-  // const [data, setData] = useState();
+  const { setAccountReference } = useAppContext()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ email: string }>({
+    defaultValues: {
+      email: '',
+    },
+  })
+  const router = useRouter()
+  const onSubmit = handleSubmit(async (data) => {
+    const res = await axios.post('/api/banxa/login', {
+      params: {
+        email: data.email,
+      },
+    })
+    setAccountReference(res.data.account_reference)
+    router.push('/create-order')
+  })
 
-  // useEffect(() => {
-  //   axios.get("/api/banxa/crypto-buy").then((response) => {
-  //     setData(response.data);
-  //   });
-  // }, []);
-
-  // return <div>{JSON.stringify(data)}</div>;
   return (
     <div className="h-full w-full bg-gradient-to-br from-[#12162e] via-[#3d5d6e] to-[#12162e] flex flex-col items-center justify-center text-white text-sm md:text-lg">
       <img src="/logo.svg" className="h-[52px] w-auto mt-[50px] md:h-[10%]" alt="banxa_logo" />
@@ -17,22 +36,38 @@ const Home: NextPage = () => {
       <div className="flex flex-col justify-center">
         <div className="text-center">Please enter your e-mail to use the Banxa app </div>
         <div className="mt-[8%] flex flex-col w-[346px] ">
-          <label htmlFor="email" className="block text-sm font-medium ">
-            Email
-          </label>
-          <div className="mt-1">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="h-[47px]  w-full rounded-md"
-              placeholder="Enter your email"
-            />
-          </div>
-
-          <button className="font-bold mt-[11px] h-[47px] w-full rounded-md bg-gradient-to-r from-[#0472c0] to-[#00d1c0] ">
-            Continue
-          </button>
+          <form onSubmit={onSubmit}>
+            <label htmlFor="email" className="block text-sm font-medium ">
+              Email
+            </label>
+            <div className="relative mt-1">
+              <input
+                type="email"
+                className="h-[47px] text-black  w-full rounded-md"
+                placeholder="Enter your email"
+                {...register('email', {
+                  required: 'Email address is required',
+                  pattern: { value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i, message: 'Email is invalid' },
+                })}
+              />
+              {errors && errors.email && (
+                <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                  <ExclamationCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
+                </div>
+              )}
+            </div>
+            {errors && (
+              <p className="mt-2 text-sm text-red-400" id="email-error">
+                {errors.email?.message}
+              </p>
+            )}
+            <button
+              type="submit"
+              className="font-bold mt-[11px] h-[47px] w-full rounded-md bg-gradient-to-r from-[#0472c0] to-[#00d1c0] "
+            >
+              Continue
+            </button>
+          </form>
         </div>
       </div>
     </div>
