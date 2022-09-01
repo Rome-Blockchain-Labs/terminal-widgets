48071a5abe05bbc791de72bea135e1af25c80c96
iandjx <iandjx@gmail.com>
2022-09-01 19:27:12 +0800

    feat: store order info in firebase on order creation

--- banxa/pages/api/banxa/create-order.ts
+++ banxa/pages/api/banxa/create-order.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from 'axios'
-import { collection } from 'firebase/firestore'
+import { collection, doc, setDoc } from 'firebase/firestore'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { postRequest } from 'utils/banxa/postRequest'
 import { PATH } from 'utils/banxa/types'
@@ -11,6 +11,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const options = postRequest(PATH.CREATE_ORDER, res, req.body.params)
   try {
     const response = await axios(options)
+    const { order } = response.data.data
+
+    await setDoc(doc(orderRef, order.id), {
+      status: 'inactive',
+      account_reference: order.account_reference,
+    })
     return res.status(200).json({ data: response.data.data })
   } catch (error) {
     const err = error as AxiosError
