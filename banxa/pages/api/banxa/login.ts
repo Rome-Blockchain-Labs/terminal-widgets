66cfedbef83b05fcf21c3bda6c86c0a279421cf6
iandjx <iandjx@gmail.com>
2022-09-01 17:09:42 +0800

    feat: store email and account ref in firebase

--- banxa/pages/api/banxa/login.ts
+++ banxa/pages/api/banxa/login.ts
@@ -1,7 +1,21 @@
+import { collection, getDoc, doc, setDoc } from 'firebase/firestore'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from 'utils/firebase'
 import { stringToHash } from '../../../utils/hash'
 
+const accountRef = collection(db, 'accounts')
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const account_reference = stringToHash(req.body.params.email)
-  return res.status(200).json({ account_reference })
+  const docSnap = await getDoc(doc(accountRef, req.body.params.email))
+
+  if (docSnap.exists()) {
+    res.status(200).json({ account_reference: docSnap.data().account_reference })
+  } else {
+    const account_reference = stringToHash(req.body.params.email)
+    await setDoc(doc(accountRef, req.body.params.email), {
+      account_reference,
+    })
+
+    res.status(200).json({ account_reference })
+  }
 }
