cc2d7fbccf41bbee81085dfcec3fe6e83e2f762b
iandjx <iandjx@gmail.com>
2022-09-01 18:04:18 +0800

    feat: wip store orderid

--- banxa/pages/api/banxa/create-order.ts
+++ banxa/pages/api/banxa/create-order.ts
@@ -1,7 +1,22 @@
+import axios, { AxiosError } from 'axios'
+import { collection } from 'firebase/firestore'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { postRequest } from 'utils/banxa/postRequest'
 import { PATH } from 'utils/banxa/types'
+import { db } from 'utils/firebase'
+
+const orderRef = collection(db, 'orders')
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await postRequest(PATH.CREATE_ORDER, res, req.body.params)
+  const options = postRequest(PATH.CREATE_ORDER, res, req.body.params)
+  try {
+    const response = await axios(options)
+    return res.status(200).json({ data: response.data.data })
+  } catch (error) {
+    const err = error as AxiosError
+    if (err.response) {
+      return res.status(err.response.status).json({ data: err.response.data })
+    }
+    return res.status(400)
+  }
 }
