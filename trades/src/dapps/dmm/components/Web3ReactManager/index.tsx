e25c1a1eb614f417e222a872442f51ad34cfc9d7
iandjx <iandjx@gmail.com>
2022-10-25 14:12:42 +0800

    use romeblockahin usewallets hook

--- trades/src/dapps/dmm/components/Web3ReactManager/index.tsx
+++ trades/src/dapps/dmm/components/Web3ReactManager/index.tsx
@@ -1,7 +1,7 @@
+import { useWeb3React } from '@romeblockchain/wallet';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-import { useWallets } from '../../../../contexts/WalletsContext/WalletContext';
 import Loader from '../Loader';
 
 const MessageWrapper = styled.div`
@@ -18,7 +18,7 @@ export default function Web3ReactManager({
 }: {
   children: JSX.Element;
 }) {
-  const { active, error } = useWallets();
+  const { isActive: active } = useWeb3React();
 
   const [showLoader, setShowLoader] = useState(false);
   useEffect(() => {
@@ -31,20 +31,8 @@ export default function Web3ReactManager({
     };
   }, []);
 
-  // if the account context isn't networkLogos, and there's an error on the network context, it's an irrecoverable error
-  if (!active && error) {
-    return (
-      <MessageWrapper>
-        <Message>
-          Oops! An unknown error occurred. Please refresh the page, or visit
-          from another browser or device.
-        </Message>
-      </MessageWrapper>
-    );
-  }
-
   // if neither context is networkLogos, spin
-  if (!active && !error) {
+  if (!active) {
     return showLoader ? (
       <MessageWrapper>
         <Loader />
