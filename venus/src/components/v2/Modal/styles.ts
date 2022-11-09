281381a8a0be9680e4041152dd9c4d891a9220f7
Eduardo Rocha <eduardorochacg@gmail.com>
2022-11-08 22:00:56 -0300

    fix: add back button on top of modal in mobile

--- venus/src/components/v2/Modal/styles.ts
+++ venus/src/components/v2/Modal/styles.ts
@@ -42,10 +42,10 @@ export const useModalStyles = ({
       overflow: auto;
       max-height: calc(100% - ${theme.spacing(8)});
 
-      padding-top: ${theme.spacing(4)};
       padding-bottom: ${theme.spacing(4)};
     `,
     titleWrapper: css`
+      padding-top: ${theme.spacing(4)};
       padding-left: ${theme.spacing(3)};
       padding-right: ${theme.spacing(3)};
       padding-bottom: ${hasTitleComponent ? theme.spacing(2) : 0};
@@ -54,8 +54,11 @@ export const useModalStyles = ({
       top: 0;
       background-color: ${hasTitleComponent ? theme.palette.background.paper : 'transparent'};
       margin-bottom: ${hasTitleComponent ? theme.spacing(4) : 0};
-      ${theme.breakpoints.down('md')} {
+      ${theme.breakpoints.down('sm')} {
         margin-bottom: ${hasTitleComponent ? theme.spacing(2) : 0};
+        padding-left: ${theme.spacing(0.5)};
+        padding-right: ${theme.spacing(0.5)};
+        padding-top: ${theme.spacing(0.5)};
       }
     `,
     titleComponent: css`
@@ -68,8 +71,8 @@ export const useModalStyles = ({
       padding-right: ${iconCloseSize};
     `,
     closeIcon: css`
-      right: ${theme.spacing(2)};
-      top: ${theme.spacing(0.5)};
+      right: ${theme.spacing(3)};
+      top: ${theme.spacing(3)};
       position: absolute;
       height: ${iconCloseSize};
       width: ${iconCloseSize};
@@ -89,6 +92,31 @@ export const useModalStyles = ({
         background-color: ${theme.palette.secondary.main};
         border-color: ${theme.palette.secondary.main};
       }
+    `,
+    mobileTitleCloseButton: css`
+      background-color: ${theme.palette.secondary.light};
+      border-color: ${theme.palette.secondary.light};
+      border-radius: 9px;
+      color: ${theme.palette.text.secondary};
+      margin-bottom: ${hasTitleComponent ? theme.spacing(0) : theme.spacing(2)};
+      text-transform: none;
+
+      :hover:not(:disabled) {
+        color: ${theme.palette.text.primary};
+        background-color: ${theme.palette.text.secondary};
+        border-color: ${theme.palette.text.secondary};
+      }
+
+      :active:not(:disabled) {
+        color: ${theme.palette.text.primary};
+        background-color: ${theme.palette.secondary.main};
+        border-color: ${theme.palette.secondary.main};
+      }
+    `,
+    mobileTitleCloseButtonIcon: css`
+      right: ${theme.spacing(2)};
+      top: ${theme.spacing(2.5)};
+      position: absolute;
     `,
     contentWrapper: css`
       padding-left: ${noHorizontalPadding ? 0 : theme.spacing(6)};
