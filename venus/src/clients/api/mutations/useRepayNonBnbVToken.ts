abb87d842ad1742e523077b4d587f74bf3161809
ian <ian@romeblockchain.com>
2022-11-21 13:29:20 +0800

    add widgetbridge events

--- venus/src/clients/api/mutations/useRepayNonBnbVToken.ts
+++ venus/src/clients/api/mutations/useRepayNonBnbVToken.ts
@@ -9,6 +9,9 @@ import {
 import FunctionKey from 'constants/functionKey';
 import { VTokenId } from 'types';
 import { useVTokenContract } from 'clients/contracts/hooks';
+import { getVBepToken } from 'utilities';
+import { useWeb3React } from '@romeblockchain/wallet';
+import { widgetBridge } from '@romeblockchain/bridge';
 
 type Options = MutationObserverOptions<
   RepayBnbOutput,
@@ -22,6 +25,9 @@ const useRepayNonBnbVToken = (
 ) => {
   const vTokenContract = useVTokenContract(vTokenId);
 
+  const vBepTokenAddress = getVBepToken(vTokenId).address;
+  const { account, chainId } = useWeb3React();
+
   return useMutation(
     FunctionKey.REPAY_NON_BNB_V_TOKEN,
     params =>
@@ -32,6 +38,14 @@ const useRepayNonBnbVToken = (
     {
       ...options,
       onSuccess: (...onSuccessParams) => {
+        if (account && chainId && options?.variables?.amountWei) {
+          widgetBridge.sendAnalyticsTxEvent('Venus_Repay_Non_BNB_Event', {
+            user_address: account,
+            chain_id: chainId.toString(),
+            token_address: vBepTokenAddress,
+            token_amount_w_decimals: options.variables.amountWei.toString(),
+          });
+        }
         queryClient.invalidateQueries(FunctionKey.GET_V_TOKEN_BALANCES_ALL);
         queryClient.invalidateQueries(FunctionKey.GET_ASSETS_IN_ACCOUNT);
         queryClient.invalidateQueries(FunctionKey.GET_MARKETS);
