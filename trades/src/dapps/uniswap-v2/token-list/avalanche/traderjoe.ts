834a090deb620fe57182252d93249d83b8a2da19
Eduardo Rocha <eduardorochacg@gmail.com>
2023-02-01 16:41:33 -0300

    fix: update link to traderjoe tokenlist

--- trades/src/dapps/uniswap-v2/token-list/avalanche/traderjoe.ts
+++ trades/src/dapps/uniswap-v2/token-list/avalanche/traderjoe.ts
@@ -1,6 +1,6 @@
 //traderjoe
 export const TRADERJOE_DEFAULT_TOKEN_LIST_URL =
-  'https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/joe.tokenlist.json';
+  'https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/mc.tokenlist.json';
 
 export const TRADER_JOE_DEFAULT_TOKEN_LIST_OF_LISTS: string[] = [
   TRADERJOE_DEFAULT_TOKEN_LIST_URL,
